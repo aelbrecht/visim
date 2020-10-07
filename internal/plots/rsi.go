3e6169ecbe91fae5c785eefb79c0a9cfee154d03
Rudolf Aelbrecht <aelbrechtrudolf@gmail.com>
2020-10-07 18:29:25 +0200

    rsi color codes

--- internal/plots/rsi.go
+++ internal/plots/rsi.go
@@ -6,6 +6,7 @@ import (
 	"github.com/hajimehoshi/ebiten/text"
 	"image"
 	"image/color"
+	"math"
 	"visim.muon.one/internal/fonts"
 	"visim.muon.one/internal/indicators"
 	"visim.muon.one/internal/stocks"
@@ -30,15 +31,21 @@ func RSI(n int, quotes []stocks.Quote, plot *image.RGBA, screen *view.Screen) {
 
 		rsi := indicators.RelativeStrengthIndex(quotes[x-n : x])
 
+		buy := math.Max(rsi-0.65, 0) / 0.35
+		sell := math.Max(1-rsi-0.65, 0) / 0.35
+
+		c := color.RGBA{
+			R: 48 + uint8((235-48)*buy) + uint8((106-48)*sell),
+			G: 51 + uint8((77-51)*buy) + uint8((176-51)*sell),
+			B: 107 + uint8((75-107)*buy) + uint8((76-107)*sell),
+			A: 150,
+		}
+
 		y := rsi * 100
 		for i := 0.0; i < y; i++ {
 			for j := 0; j < 3; j++ {
-				plot.Set((x-screen.Camera.X)*3+j, int(i), color.RGBA{190, 46, 221, 40})
+				plot.Set((x-screen.Camera.X)*3+j, int(i), c)
 			}
-		}
-
-		for j := 0; j < 3; j++ {
-			plot.Set((x-screen.Camera.X)*3+j, 100, color.RGBA{190, 46, 221, 60})
 		}
 	}
 
